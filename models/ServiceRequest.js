2e43eaad9d8e7191f05687fb91f41da1385f2382
gsaiteja001 <saiteja244139@gmail.com>
2024-11-28 02:12:12 +0530

    Create ServiceRequest.js

---
+++ models/ServiceRequest.js
@@ -0,0 +1,64 @@
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const ServiceProvider = require('./ServiceProvider');
+const Service = require('./Service');
+const Address = require('./Address');
+
+const ServiceRequest = sequelize.define('ServiceRequest', {
+  RequestID: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+  },
+  FarmerName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  FarmerContactInfo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  ScheduledDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  AddressID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Address,
+      key: 'AddressID',
+    },
+  },
+  ServiceProviderID: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+      model: ServiceProvider,
+      key: 'ProviderID',
+    },
+  },
+  ServiceID: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+      model: Service,
+      key: 'ServiceID',
+    },
+  },
+  Status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'pending', // Other statuses: 'accepted', 'completed', 'canceled'
+  },
+  Notes: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+}, {
+  tableName: 'ServiceRequest',
+  timestamps: true,
+});
+
+module.exports = ServiceRequest;
