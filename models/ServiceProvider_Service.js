7843a28b9bbc2ad431a1129faaf31830409d4d81
gsaiteja001 <saiteja244139@gmail.com>
2024-11-28 14:28:29 +0530

    Update ServiceProvider_Service.js

--- models/ServiceProvider_Service.js
+++ models/ServiceProvider_Service.js
@@ -1,49 +1,30 @@
 // models/ServiceProvider_Service.js
+
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-const ServiceProvider = require('./ServiceProvider');
-const Service = require('./Service');
 
 const ServiceProvider_Service = sequelize.define('ServiceProvider_Service', {
   ProviderID: {
     type: DataTypes.STRING,
     primaryKey: true,
     references: {
-      model: ServiceProvider,
+      model: 'ServiceProvider', // Note: Using table name as string
       key: 'ProviderID',
     },
+    allowNull: false,
   },
   ServiceID: {
     type: DataTypes.STRING,
     primaryKey: true,
     references: {
-      model: Service,
+      model: 'Service', // Note: Using table name as string
       key: 'ServiceID',
     },
+    allowNull: false,
   },
 }, {
   tableName: 'ServiceProvider_Service',
   timestamps: false, // Disable timestamps
 });
-
-// // Ensure associations are defined in both models
-// ServiceProvider.belongsToMany(Service, {
-//   through: {
-//     model: ServiceProvider_Service,
-//     timestamps: false,
-//   },
-//   foreignKey: 'ProviderID',
-//   otherKey: 'ServiceID',
-// });
-
-
-// Service.belongsToMany(ServiceProvider, {
-//   through: {
-//     model: ServiceProvider_Service,
-//     timestamps: false,
-//   },
-//   foreignKey: 'ServiceID',
-//   otherKey: 'ProviderID',
-// });
 
 module.exports = ServiceProvider_Service;
