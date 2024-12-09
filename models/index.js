79aad8b8d65993da1e04c14dea6db639ab041dae
gsaiteja001 <saiteja244139@gmail.com>
2024-12-10 00:24:55 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,50 +1,18 @@
 // models/index.js
-
-
-const sequelize = require('../config/database');
-const { Sequelize } = require('sequelize');
-
-// Import Models
 const ServiceProvider = require('./ServiceProvider');
 const Service = require('./Service');
-const ServiceProvider_Service = require('./ServiceProvider_Service');
 const Address = require('./Address');
 const Equipment = require('./Equipment');
-const Farmer = require('./farmer'); 
+const Crop = require('./Crop');
+const ServiceRequest = require('./ServiceRequest');
+const Farmer = require('./Farmer'); // Ensure this model is defined
 
-// Define Associations
-
-// ServiceProvider <-> Service (Many-to-Many)
-ServiceProvider.belongsToMany(Service, {
-  through: ServiceProvider_Service,
-  foreignKey: 'ProviderID',
-  otherKey: 'ServiceID',
-});
-Service.belongsToMany(ServiceProvider, {
-  through: ServiceProvider_Service,
-  foreignKey: 'ServiceID',
-  otherKey: 'ProviderID',
-});
-
-// ServiceProvider -> Equipment (One-to-Many)
-ServiceProvider.hasMany(Equipment, { foreignKey: 'OwnedBy' });
-Equipment.belongsTo(ServiceProvider, { foreignKey: 'OwnedBy' });
-
-// ServiceProvider -> Address (One-to-Many)
-ServiceProvider.hasMany(Address, { foreignKey: 'ProviderID' });
-Address.belongsTo(ServiceProvider, { foreignKey: 'ProviderID' });
-
-
-
-// Export Models
 module.exports = {
-  sequelize,
   ServiceProvider,
   Service,
-  ServiceProvider_Service,
   Address,
   Equipment,
+  Crop,
+  ServiceRequest,
   Farmer,
-  sequelize,
-  Sequelize,
 };
