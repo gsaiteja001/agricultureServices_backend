82dad6517ee561e3e61cbfb359c1500854473583
gsaiteja001 <saiteja244139@gmail.com>
2024-12-10 08:15:44 +0530

    Update ServiceProvider.js

--- models/ServiceProvider.js
+++ models/ServiceProvider.js
@@ -1,51 +1,20 @@
 // models/ServiceProvider.js
+
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const ServiceProviderSchema = new Schema({
-  providerID: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  contactInfo: {
-    type: String,
-    required: true,
-  },
-  availability: {
-    type: String,
-    required: true,
-  },
-  experience: {
-    type: String,
-    required: true,
-  },
-  certifications: {
-    type: String,
-    required: true,
-  },
-  ratings: {
-    type: Number,
-    required: true,
-  },
-  farmerId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  // Adding references to related collections
-  addresses: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Address',
-  }],
-  equipment: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Equipment',
-  }],
+  providerID: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  contactInfo: { type: String, required: true },
+  availability: { type: String, required: true },
+  experience: { type: String, required: true },
+  certifications: { type: String, required: true },
+  ratings: { type: Number, required: true, min: 0, max: 5 },
+  farmerId: { type: String, required: true, unique: true },
+  services: [{ type: Schema.Types.ObjectId, ref: 'Service' }], // Many-to-Many
+  equipments: [{ type: Schema.Types.ObjectId, ref: 'Equipment' }], // One-to-Many
+  addresses: [{ type: Schema.Types.ObjectId, ref: 'Address' }], // One-to-Many
 }, {
   collection: 'ServiceProvider',
   timestamps: false,
