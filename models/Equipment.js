bc8b3dde0c00c2c2555d1a5a3f4fd42313045ace
gsaiteja001 <saiteja244139@gmail.com>
2024-12-10 08:17:07 +0530

    Update Equipment.js

--- models/Equipment.js
+++ models/Equipment.js
@@ -1,36 +1,17 @@
 // models/Equipment.js
+
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { v4: uuidv4 } = require('uuid');
 
 const EquipmentSchema = new Schema({
-  equipmentID: {
-    type: String,
-    default: uuidv4,
-    unique: true,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    default: null,
-  },
-  description: {
-    type: String,
-    default: null,
-  },
-  capacity: {
-    type: String,
-    default: null,
-  },
-  ownedBy: {
-    type: Schema.Types.ObjectId,
-    ref: 'ServiceProvider',
-    required: true,
-  },
+  equipmentID: { type: String, default: uuidv4, unique: true, required: true },
+  name: { type: String, required: true },
+  type: { type: String, default: null },
+  description: { type: String, default: null },
+  capacity: { type: String, default: null },
+  serviceProvider: { type: Schema.Types.ObjectId, ref: 'ServiceProvider', required: true }, // One-to-Many
+  crops: [{ type: Schema.Types.ObjectId, ref: 'Crop' }], // Many-to-Many
 }, {
   collection: 'Equipment',
   timestamps: false,
