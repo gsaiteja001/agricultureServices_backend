33fc0e13228e06f9d73f402ddaa3bbbffaf6134a
gsaiteja001 <saiteja244139@gmail.com>
2024-12-10 10:09:14 +0530

    Update Service.js

--- models/Service.js
+++ models/Service.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const ServiceSchema = new Schema({
-  serviceID: { type: String, required: true, unique: true },
-  serviceName: { type: String, required: true },
-  category: { type: String, required: true },
+  serviceID: { type: String, required: false, unique: true },
+  serviceName: { type: String, required: false },
+  category: { type: String, required: false },
   description: { type: String, default: null },
-  serviceProviders: [{ type: Schema.Types.ObjectId, ref: 'ServiceProvider' }], // Many-to-Many
+  serviceProviders: [{ type: Schema.Types.ObjectId, ref: 'ServiceProvider', required: false }], // Many-to-Many
 }, {
   collection: 'Service',
   timestamps: false,
